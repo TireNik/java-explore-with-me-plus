14a8034e36c485a53b2d6d65a6c1a6da19529949
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-03-19 15:32:58 +0300

    Добавил сущность User

---
+++ main-service/src/main/java/ru/practicum/model/User.java
@@ -0,0 +1,22 @@
+package ru.practicum.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Data
+@Table(name = "users")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String email;
+}
