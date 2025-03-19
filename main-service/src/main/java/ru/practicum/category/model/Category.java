45a80a4315288fce3de2f78dd9ca2b697f4a8f93
Aliona Kulikova <kulikovaaliona888@yandex.ru>
2025-03-19 16:43:35 +0300

    добавила категории

---
+++ main-service/src/main/java/ru/practicum/category/model/Category.java
@@ -0,0 +1,19 @@
+package ru.practicum.category.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categories")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+}
