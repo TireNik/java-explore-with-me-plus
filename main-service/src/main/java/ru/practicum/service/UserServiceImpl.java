5189eeb9059eec9f5639e1f3aa5e9f7bf0958e28
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-03-19 17:43:26 +0300

    Добавил UserServiceImpl

---
+++ main-service/src/main/java/ru/practicum/service/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package ru.practicum.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import ru.practicum.dto.NewUserRequest;
+import ru.practicum.dto.UserDto;
+import ru.practicum.error.ConflictException;
+import ru.practicum.error.NotFoundException;
+import ru.practicum.error.ValidationException;
+import ru.practicum.mapper.UserMapper;
+import ru.practicum.model.User;
+import ru.practicum.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public List<UserDto> getUsers(List<Long> ids, Integer from, Integer size) {
+        if (from < 0 || size <= 0) {
+            throw new ValidationException("Параметры пагинации должны быть неотрицательными и size > 0");
+        }
+        Pageable pageable = PageRequest.of(from / size, size);
+        List<User> users = (ids == null || ids.isEmpty()) ?
+                userRepository.findAll(pageable).getContent() :
+                userRepository.findByIdIn(ids, pageable).getContent();
+        return users.stream()
+                .map(userMapper::toUserDto)
+                .toList();
+    }
+
+    @Override
+    public UserDto create(NewUserRequest newUser) {
+        if (userRepository.existsByEmail(newUser.getEmail())) {
+            throw new ConflictException("Пользователь с email=" + newUser.getEmail() + " уже существует");
+        }
+        User user = userMapper.toUser(newUser);
+        return userMapper.toUserDto(userRepository.save(user));
+    }
+
+    @Override
+    public void delete(Long id) {
+        checkUserExists(id);
+        userRepository.deleteById(id);
+    }
+
+    private void checkUserExists(Long id) {
+        userRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Пользователь не найден или недоступен"));
+    }
+}
