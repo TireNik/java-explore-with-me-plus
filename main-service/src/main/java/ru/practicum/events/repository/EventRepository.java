c4023a1b016f609b00016b46d704cfed0292432f
TireNik <sugarman.sviridov@yandex.ru>
2025-03-19 20:17:18 +0300

    add getPublicEvents by id

--- main-service/src/main/java/ru/practicum/events/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/events/repository/EventRepository.java
@@ -3,6 +3,12 @@ package ru.practicum.events.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ru.practicum.events.model.Event;
+import ru.practicum.events.model.EventState;
+
+import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
+
+    Optional<Event> findByIdAndState(Long id, EventState state);
+
 }
