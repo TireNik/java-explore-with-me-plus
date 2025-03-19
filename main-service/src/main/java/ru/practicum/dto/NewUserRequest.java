a3d61ecf52d5074e025162ea4065de44219c608b
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-03-19 15:43:58 +0300

    Добавил NewUserRequest

---
+++ main-service/src/main/java/ru/practicum/dto/NewUserRequest.java
@@ -0,0 +1,23 @@
+package ru.practicum.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class NewUserRequest {
+    @NotBlank
+    @Size(min = 2, max = 250)
+    String name;
+
+    @NotBlank
+    @Size(min = 2, max = 254)
+    String email;
+}
