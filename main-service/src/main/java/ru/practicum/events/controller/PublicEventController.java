d8f336597680247898661850e6914a8a8633f60d
TireNik <sugarman.sviridov@yandex.ru>
2025-03-19 16:51:29 +0300

    PublicEventController

---
+++ main-service/src/main/java/ru/practicum/events/controller/PublicEventController.java
@@ -0,0 +1,12 @@
+package ru.practicum.events.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+@RequiredArgsConstructor
+public class PublicEventController {
+}
