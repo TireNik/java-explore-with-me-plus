2d39dcffa3acbd780eaf080ffcdaa260db976213
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-03-20 16:35:13 +0300

    Добавил методы в CategoryRepository

--- main-service/src/main/java/ru/practicum/category/repository/CategoryRepository.java
+++ main-service/src/main/java/ru/practicum/category/repository/CategoryRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.category.model.Category;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+    boolean existsByName(String name);
+    Optional<Category> findByName(String name);
 }
