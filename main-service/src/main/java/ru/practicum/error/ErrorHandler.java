318d69643cb8676576ac3298e21ac677363d19f5
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-03-20 21:28:13 +0300

    Убрал необязательные комментарии из ErrorHandler

--- main-service/src/main/java/ru/practicum/error/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/error/ErrorHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.error.exception.ConflictException;
-import ru.practicum.error.exception.ForbiddenOperationException; // Добавь импорт
+import ru.practicum.error.exception.ForbiddenOperationException;
 import ru.practicum.error.exception.NotFoundException;
 import ru.practicum.error.exception.ResourceNotFoundException;
 
