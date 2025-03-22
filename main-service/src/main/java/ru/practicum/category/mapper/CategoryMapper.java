686bf292e080f8c2a49931e79a205cdf74979b76
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-03-22 11:53:43 +0300

    Добавил @Mapping в CategoryMapper

--- main-service/src/main/java/ru/practicum/category/mapper/CategoryMapper.java
+++ main-service/src/main/java/ru/practicum/category/mapper/CategoryMapper.java
@@ -1,6 +1,7 @@
 package ru.practicum.category.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.category.dto.CategoryDto;
 import ru.practicum.category.dto.CategoryDtoNew;
 import ru.practicum.category.model.Category;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
+    @Mapping(target = "id", ignore = true)
     Category toCategory(CategoryDtoNew categoryDtoNew);
 
     CategoryDto toCategoryDto(Category category);
