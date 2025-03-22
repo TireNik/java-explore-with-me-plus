3ec06e767c47923ef68732cc66df7b391f3ab09b
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-03-22 13:30:24 +0300

    Добавил аннотацию @Builder.Default в CompilationDtoRequest для прохождения валидации кода

--- main-service/src/main/java/ru/practicum/compilation/dto/CompilationDtoRequest.java
+++ main-service/src/main/java/ru/practicum/compilation/dto/CompilationDtoRequest.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CompilationDtoRequest {
+    @Builder.Default
     private List<Long> events = new ArrayList<>();
     private Boolean pinned;
 
