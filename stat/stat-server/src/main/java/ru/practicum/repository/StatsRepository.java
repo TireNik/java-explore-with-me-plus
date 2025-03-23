9a47ca6df0fc4f450a0871f797d22c00e4589668
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-03-23 22:07:19 +0300

    Убрал неиспользуемые импорты из StatsRepository

--- stat/stat-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stat/stat-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.practicum.model.Stat;
 
 import java.time.LocalDateTime;
