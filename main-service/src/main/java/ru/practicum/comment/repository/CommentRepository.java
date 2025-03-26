1728ebb7ec2d9f6e266c588085d6a1333f7330d3
TireNik <sugarman.sviridov@yandex.ru>
2025-03-26 19:10:13 +0300

    feature 2.0

--- main-service/src/main/java/ru/practicum/comment/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package ru.practicum.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.comment.model.Comment;
 
@@ -11,6 +12,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> getCommentsByAuthorId(Long userId);
 
+    @Query("SELECT c FROM Comment c WHERE c.event.id = ?1 ORDER BY c.id LIMIT ?3 OFFSET ?2")
     List<Comment> findAllCommentsForEvent(Long eventId, Integer from, Integer size);
 
 }
