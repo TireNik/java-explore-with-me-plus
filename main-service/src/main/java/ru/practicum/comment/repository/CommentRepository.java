66e5fb150b678808271e4df67941f52e1cd2cf5c
Aliona Kulikova <kulikovaaliona888@yandex.ru>
2025-03-26 18:48:30 +0300

    изменила запрос

--- main-service/src/main/java/ru/practicum/comment/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/comment/repository/CommentRepository.java
@@ -1,6 +1,8 @@
 package ru.practicum.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.comment.model.Comment;
 
@@ -13,5 +15,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> getCommentsByAuthorId(Long userId);
 
-    List<Comment> findAllCommentsForEvent(Long eventId, Pageable pageable);
+    @Query("SELECT c FROM Comment c WHERE c.event.id = :eventId")
+    List<Comment> findAllCommentsForEvent(@Param("eventId") Long eventId, Pageable pageable);
 }
